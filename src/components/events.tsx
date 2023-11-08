668c8b472ce0fb7b34857302e97d7742a4c710c1
Menake Ratnayake <menake142@gmail.com>
2023-11-09 10:06:58 +1300

    Feature/update home page (#3)
    * Added transport informatio to home page
    
    * Tidy ups
    
    * updated bus times

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -1,92 +1,64 @@
-import Link from "next/link";
-import type { Invitation } from "@/invitation-provider";
 import { useInvitation } from "@/invitation-provider";
-import { ArrowRight } from "lucide-react";
-
-const getRsvpUrlFromResponseStage = (invitation: Invitation) => {
-  if (!invitation.id) return "";
-
-  const firstEvent = invitation.events[0];
-
-  if (invitation.responseStage === "NORESPONSE")
-    return firstEvent?.eventType === "HINDU_CEREMONY"
-      ? `/rsvp/${invitation.id}/ceremony`
-      : `/rsvp/${invitation.id}/reception`;
-
-  if (invitation.responseStage === "HINDU_CEREMONY")
-    return invitation.events.length > 1
-      ? `/rsvp/${invitation.id}/reception`
-      : `/rsvp/${invitation.id}/ceremony`;
-
-  if (invitation.responseStage === "RECEPTION")
-    return `/rsvp/${invitation.id}/reception/transport`;
-
-  return `/rsvp/${invitation.id}/reception/transport`;
-};
+import { api } from "@/utils/api";
 
 export const Events = () => {
   const invitation = useInvitation();
+  const { data } = api.rsvp.transport.useQuery();
 
-  const firstEvent = invitation.events[0];
-  const secondEvent = invitation.events[1];
+  const poruwaEvent = invitation.events.find(
+    (event) => event.eventType === "PORUWA_AND_RECEPTION"
+  );
+  console.log(data);
 
   return (
     <div className="flex w-full flex-col justify-between italic sm:justify-center">
-      {firstEvent && (
-        <div className="my-8 w-full border-l border-stone-100 pt-3 pb-16 pl-5 text-white sm:w-1/2 sm:pl-10">
+      {poruwaEvent && (
+        <div className="my-8 w-full border-l border-stone-100 pt-3 pb-8 pl-5 text-white sm:pl-10">
           <div className="text-xl uppercase text-white sm:text-2xl">
-            {firstEvent.name}
+            {poruwaEvent.name}
           </div>
-          <div className="mt-5 ">{firstEvent.venue.name}</div>
+          <div className="mt-5 ">{poruwaEvent.venue.name}</div>
           <div>
-            {firstEvent.date.toLocaleDateString("en-nz", {
+            {poruwaEvent.date.toLocaleDateString("en-nz", {
               day: "numeric",
               weekday: "long",
               month: "long",
               year: "numeric",
             })}
           </div>
-          <div>{firstEvent.time}</div>
+          <div>{poruwaEvent.time}</div>
 
-          <div>{firstEvent.dressCode}</div>
+          <div>{poruwaEvent.dressCode}</div>
         </div>
       )}
-
-      {secondEvent && (
-        <div className="my-8 flex items-end justify-end text-right">
-          <div className="w-3/4 border-r border-stone-100 pb-3 pt-16 pr-5 sm:w-1/2 sm:pr-10">
-            <div className="text-xl uppercase sm:text-2xl">
-              {secondEvent.name}
-            </div>
-            <div className="mt-5">{secondEvent.venue.name}</div>
-            <div>
-              {secondEvent.date.toLocaleDateString("en-nz", {
-                day: "numeric",
-                weekday: "long",
-                month: "long",
-                year: "numeric",
-              })}
-            </div>
-            <div>{secondEvent.time}</div>
-
-            <div>{secondEvent.dressCode}</div>
-          </div>
+      {data && data.numberOfSeats > 0 ? (
+        <div className="mx-5 mb-10 mt-2 italic">
+          <span>
+            You have indicated that you would like to take one of the available
+            buses to Markovina Valley Estate. The timetable is below, please be
+            on time as a courtesy to other guests. We look forward to
+            celebrating with you!
+          </span>
+          <div className="mt-8 text-lg">To Markovina (arriving at 4pm)</div>
+          <ul className="mt-1 px-5">
+            <li>Leaving Pakuranga at 2:50pm</li>
+            <li>Leaving Newmarket at 3:20pm</li>
+          </ul>
+          <div className="mt-6 text-lg">From Markovina (leaving at 12pm)</div>
+          <ul className="mt-1 px-5">
+            <li>Arriving at Newmarket</li>
+            <li>Arriving at Pakuranga</li>
+          </ul>
+        </div>
+      ) : (
+        <div className="mx-5 mt-2 italic">
+          <span>
+            You have indicated that you will not be needing the provided bus
+            service. Parking is available on site at Markovina Valley Estate,
+            please drive safely and we look forward to celebrating with you!
+          </span>
         </div>
       )}
-
-      <div
-        className={`my-8 ml-5 flex flex-row  ${
-          invitation.events.length > 1 ? "justify-start" : "justify-end"
-        }`}
-      >
-        <Link
-          className="flex flex-row text-xl sm:text-2xl"
-          href={getRsvpUrlFromResponseStage(invitation)}
-        >
-          RSVP
-          <ArrowRight className="ml-2" />
-        </Link>
-      </div>
     </div>
   );
 };
